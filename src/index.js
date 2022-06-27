642cd3d2cc3e9d371cd8c9f1ecc43f776eee6bfa
fractal161 <628snapdragon@gmail.com>
2022-06-27 17:26:59 -0500

    add default servers, g r i d

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import InviteCard from './inviteCard/InviteCard';
 import reportWebVitals from './reportWebVitals';
+import servers from './servers.json'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <InviteCard code="qXZUKZzv7a"/>
+    {servers.map(server => {
+      return <InviteCard code={server}></InviteCard>
+    })};
+
   </React.StrictMode>
 );
 
