dc85d9226f322cb7394e371e7db57774ffb1e10e
fractal161 <628snapdragon@gmail.com>
2022-06-30 00:43:39 -0500

    poorly steal discord palette

--- src/components/InviteCard.js
+++ src/components/InviteCard.js
@@ -6,26 +6,26 @@ function InviteCard({code}) {
   const [name, setName] = useState('Server Name');
   const [iconUrl, setIcon] = useState('');
   const [memberCount, setMemberCount] = useState(0);
-  const fetchData = async () => {
-    const res = await axios(
-      `https://discord.com/api/v10/invites/${code}?with_counts=true`,
-    );
-    const guild = res.data.guild;
-    setName(guild.name);
-    setIcon(["https://cdn.discordapp.com/icons", guild.id, guild.icon].join('/'));
-    setMemberCount(res.data.approximate_member_count)
-  };
   useEffect(() => {
+    const fetchData = async () => {
+      const res = await axios(
+        `https://discord.com/api/v10/invites/${code}?with_counts=true`,
+      );
+      const guild = res.data.guild;
+      setName(guild.name);
+      setIcon(["https://cdn.discordapp.com/icons", guild.id, guild.icon].join('/'));
+      setMemberCount(res.data.approximate_member_count)
+    };
     fetchData();
-  }, []);
+  }, [code]);
   return (
     <div className="InviteCard">
-      <img src={iconUrl} alt="icon" height={48} />
+      <img className="icon" src={iconUrl} alt="icon" height={48} />
       <div className="InviteCard-text">
         <h2>{name}</h2>
         <p id="members">{memberCount} Members</p>
       </div>
-      <button className="InviteCard-button" name="Join">
+      <button className="invite-button" name="Join">
         Join
       </button>
     </div>
