a6a771659cedb4c4df001da7c591d5c380505c9e
fractal161 <628snapdragon@gmail.com>
2022-07-08 01:31:35 -0500

    query guild info from postgres

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,10 +1,20 @@
 var express = require('express');
+var db = require('../db');
 var servers = require('../public/servers.json');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.json(servers);
+  db.query(
+    `SELECT id, code, name, icon, members, description from servers`,
+    (err, result) => {
+      if (err) {
+        console.log(err.stack);
+      } else {
+        res.send(result.rows);
+      }
+    }
+  );
 });
 
 module.exports = router;
